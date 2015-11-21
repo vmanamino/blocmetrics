ce4cb8551eeb16ed7bd9eabeb6a0b9a1520824d0
Viral Amin <vmanamino@gmail.com>
2015-11-21 02:14:19 +0000

    route for events

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
+
+  namespace :api, defaults: { format: :json } do
+    resources :events, only: [:create]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
