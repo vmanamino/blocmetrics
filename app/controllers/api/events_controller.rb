36c4f6be106526483f372325b4f04cea5179b436
Viral Amin <vmanamino@gmail.com>
2015-11-21 06:45:12 +0000

    api events controller

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,26 @@
+class API::EventsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Content-Type'
+  end
+
+  def create
+    event = Event.create(event_params)
+    event.registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+    if event.save
+      render json: event, status: :created
+    else
+      render event.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#index', via: [:options]
     resources :events, only: [:create]
   end
   # The priority is based upon order of creation: first created -> highest priority.
