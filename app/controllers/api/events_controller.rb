cf33a033ee31670bd3933955f90b610f5af087dc
Viral Amin <vmanamino@gmail.com>
2015-11-25 19:02:19 +0000

    set_access_control_headers private

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,12 +2,6 @@ class API::EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_filter :set_access_control_headers
 
-  def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'Content-Type'
-  end
-
   def create
     event = Event.create(event_params)
     event.registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
@@ -22,5 +16,11 @@ class API::EventsController < ApplicationController
 
   def event_params
     params.permit(:name)
+  end
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Content-Type'
   end
 end
