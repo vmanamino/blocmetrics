7a29a9481332e51a6a65558eea5c3b5bf597e211
Viral Amin <vmanamino@gmail.com>
2015-11-25 17:50:59 +0000

    rack cors config

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Blocmetrics
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '/api/events', :headers => :any, :methods => [:get, :post, :options]
       end
     end
   end
